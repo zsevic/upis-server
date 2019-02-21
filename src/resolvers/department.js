e6e9b6f4dc9578872c47f6ffad491db2ec6f97ef
zsevic <zsevic95@gmail.com>
2019-02-21 05:27:51 +0100

    fix(department resolver): fix department resolver for faculty

--- src/resolvers/department.js
+++ src/resolvers/department.js
@@ -1,13 +1,15 @@
 export default {
   Query: {
     department: async (parent, { id }, { models }) => {
-      return models.Department.findByPk(id)
-    }
+      return models.Department.findByPk(id);
+    },
   },
 
   Department: {
-    faculty: async (parent, { id }, { models }) => {
-      return models.Faculty.findByPk(id)
-    }
-  }
-}
+    faculty: async (department, { id }, { models }) => {
+      return models.Faculty.findOne({
+        where: { id: department.facultyId },
+      });
+    },
+  },
+};
