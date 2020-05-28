aaf2459cdb5e40a556fb6a2a7fc6475387baa13a
zsevic <zsevic95@gmail.com>
2020-05-28 02:13:29 +0200

    Format queries

--- test/api.js
+++ test/api.js
@@ -1,35 +1,35 @@
 export const user = async (variables) => api.post('/graphql').send({
   query: `
-            query ($id: ID!) {
-                user(id:$id) {
-                    id
-                    username
-                    email
-                    role
-                }
-            }
-        `,
+    query ($id: ID!) {
+      user(id:$id) {
+        id
+        username
+        email
+        role
+      }
+    }
+  `,
   variables,
 });
 
 export const signIn = async (variables) => api.post('/graphql').send({
   query: `
-        mutation ($login: String!, $password: String!) {
-            signIn(login:$login, password:$password) {
-                token
-            }
-        }
-      `,
+    mutation ($login: String!, $password: String!) {
+      signIn(login:$login, password:$password) {
+        token
+      }
+    }
+  `,
   variables,
 });
 
 export const deleteUser = async (variables, token) => api.post('/graphql').send(
   {
     query: `
-        mutation($id:ID!) {
-            deleteUser(id:$id)
-        }
-      `,
+      mutation($id:ID!) {
+        deleteUser(id:$id)
+      }
+    `,
     variables,
   },
   {
